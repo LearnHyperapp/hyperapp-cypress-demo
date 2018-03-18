b36febc7461e0ffcbe1522889d2e801ab6f96715
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-03-18 12:15:17 -0400

    add todo-item view function tests

--- cypress/integration/todo_item_spec.js
+++ cypress/integration/todo_item_spec.js
@@ -1,20 +1,39 @@
+/// <reference types="cypress" />
 import { h } from 'hyperapp'
 import { mount } from 'cypress-hyperapp-unit-test'
 import TodoItem from '../../src/todo-item'
 
 describe('todo item', () => {
-  beforeEach(() => {
-    const state = {}
-    const actions = {}
+  it('shows an item', () => {
+    const view = (state, actions) => h(TodoItem, {
+      id: '1',
+      value: 'test item',
+      done: false
+    })
+    mount(null, null, view)
+    cy.contains('test item')
+  })
+
+  it('marks done items', () => {
+    const view = (state, actions) => h(TodoItem, {
+      id: '1',
+      value: 'test item',
+      done: true
+    })
+    mount(null, null, view)
+    cy.contains('test item').should('have.class', 'done')
+  })
+
+  it('calls toggle on click', () => {
+    const toggle = cy.spy().as('toggle')
     const view = (state, actions) => h(TodoItem, {
       id: '1',
       value: 'test item',
       done: false,
-      toggle: () => {}
+      toggle
     })
-    mount(state, actions, view)
-  })
-
-  it('calls toggle on click', () => {
+    mount(null, null, view)
+    cy.contains('test item').click()
+    cy.get('@toggle').should('be.calledOnce')
   })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,23 +1,5 @@
 const webpackPreprocessor = require('@cypress/webpack-preprocessor')
-// hmm, why is the common webpack config leading to React.createElement?
-// const webpackOptions = require('../../webpack.config.js')
-
-const webpackOptions = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env'],
-          plugins: []
-        }
-      }
-    ]
-  }
-}
-
+const webpackOptions = require('../../webpack.config.js')
 module.exports = on => {
   on('file:preprocessor', webpackPreprocessor({ webpackOptions }))
 }
